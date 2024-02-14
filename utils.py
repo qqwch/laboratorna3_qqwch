e960e7a05270b8776d8babbff56955bb97ef875a
Ivan Yakusha <ivanakusa064@gmail.com>
2024-02-14 20:03:16 +0200

    Add new function

--- utils.py
+++ utils.py
@@ -3,3 +3,13 @@ def factorial(n):
         return 1
     else:
         return n*factorial(n-1)
+
+def is_prime(n):
+    if n < 2:
+        return 0
+    else:
+        for i in range(2, int(n * 0.5) + 1):
+            if n % i == 0:
+                return "Not prime"
+        else:
+            return "Is prime"
